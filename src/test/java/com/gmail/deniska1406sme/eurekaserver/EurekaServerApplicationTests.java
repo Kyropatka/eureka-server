69dcbdd57f41e8e83f8eb240b424dba1aca55f19
Denys Smelnytskyi <deniska1406sme@gmail.com>
2025-05-15 20:07:04 +0300

    Initial commit

---
+++ src/test/java/com/gmail/deniska1406sme/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.deniska1406sme.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
